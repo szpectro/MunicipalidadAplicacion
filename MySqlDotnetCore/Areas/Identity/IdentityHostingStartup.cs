c65abc874800efe4e0a1b7c8ccd1216ff10b6303
szpectro <sebastiancovarrubiasg@hotmail.com>
2021-12-23 08:10:04 -0300

    Clave unica

---
+++ MySqlDotnetCore/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MySqlDotnetCore.Data;
+
+[assembly: HostingStartup(typeof(MySqlDotnetCore.Areas.Identity.IdentityHostingStartup))]
+namespace MySqlDotnetCore.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
